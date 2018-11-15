702b0c6ec1ceb85a53a8f5dee2aebfd1035f379b
sancher6 <sancher6@tcnj.edu>
2018-11-14 21:53:52 -0500

    WORKING APP WITH LIGHTS

--- raspberrypiclient/RaspberryPiClient/app/src/main/java/emaraic/com/raspberrypiclient/MainActivity.java
+++ raspberrypiclient/RaspberryPiClient/app/src/main/java/emaraic/com/raspberrypiclient/MainActivity.java
@@ -3,18 +3,23 @@ package emaraic.com.raspberrypiclient;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,6 +30,7 @@ import java.util.regex.Pattern;
  *         Email : sancher6@tcnj.edu
  *         Created on: 10/29/2018
  */
+
 public class MainActivity extends AppCompatActivity {
     private Switch led1;
     private Switch led2;
@@ -45,88 +51,96 @@ public class MainActivity extends AppCompatActivity {
                     "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
                     "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
 
-
-    @Override
+@Override
     protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        pattern = Pattern.compile(IPADDRESS_PATTERN);
-        handler = new Handler();
-        setContentView(R.layout.activity_main);
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            //your codes here
+            super.onCreate(savedInstanceState);
+            pattern = Pattern.compile(IPADDRESS_PATTERN);
+            handler = new Handler();
+            setContentView(R.layout.activity_main);
 
-        led1 = (Switch) findViewById(R.id.led1);
-        led2 = (Switch) findViewById(R.id.led2);
-        led3 = (Switch) findViewById(R.id.led3);
-        ip = (EditText) findViewById(R.id.ip);
-        port = (EditText) findViewById(R.id.port);
-        connect = (Button) findViewById(R.id.connect);
+            led1 = (Switch) findViewById(R.id.led1);
+            led2 = (Switch) findViewById(R.id.led2);
+            led3 = (Switch) findViewById(R.id.led3);
+            ip = (EditText) findViewById(R.id.ip);
+            port = (EditText) findViewById(R.id.port);
+            connect = (Button) findViewById(R.id.connect);
 
-        changeSwitchesSatte(false);
+            changeSwitchesSatte(false);
 
-        connect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (connect.getText().toString().equalsIgnoreCase("Connect")) {
-                    try {
-                        ipaddress = ip.getText().toString();
-                        if (!checkIP(ipaddress))
-                            throw new UnknownHostException(port + "is not a valid IP address");
-                        portnum = Integer.parseInt(port.getText().toString());
-                        if (portnum > 65535 && portnum < 0)
-                            throw new UnknownHostException(port + "is not a valid port number ");
-                        Client client = new Client(ipaddress, portnum);
-                        client.start();
+            connect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (connect.getText().toString().equalsIgnoreCase("Connect")) {
+                        try {
+                            ipaddress = ip.getText().toString();
+                            if (!checkIP(ipaddress))
+                                throw new UnknownHostException(port + "is not a valid IP address");
+                            portnum = Integer.parseInt(port.getText().toString());
+                            if (portnum > 65535 && portnum < 0)
+                                throw new UnknownHostException(port + "is not a valid port number ");
+                            Client client = new Client(ipaddress, portnum);
+                            client.start();
 
 
-                    } catch (UnknownHostException e) {
-                        showErrorsMessages("Please enter a valid IP !! ");
-                    } catch (NumberFormatException e) {
-                        showErrorsMessages("Please enter valid port number !! ");
+                        } catch (UnknownHostException e) {
+                            showErrorsMessages("Please enter a valid IP !! ");
+                        } catch (NumberFormatException e) {
+                            showErrorsMessages("Please enter valid port number !! ");
+                        }
+                    } else {
+                        connect.setText("Connect");
+                        changeSwitchesSatte(false);
+                        closeConnection();
                     }
-                } else {
-                    connect.setText("Connect");
-                    changeSwitchesSatte(false);
-                    closeConnection();
                 }
-            }
-        });
-        led1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-                if (isChecked) {
-                    lightOn(1);
-                } else {
-                    lightOff(1);
+            });
+            led1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                                             boolean isChecked) {
+                    if (isChecked) {
+                        lightOn(1);
+                    } else {
+                        lightOff(1);
+                    }
                 }
-            }
-        });
+            });
 
-        led2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-                if (isChecked) {
-                    lightOn(2);
-                } else {
-                    lightOff(2);
+            led2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                                             boolean isChecked) {
+                    if (isChecked) {
+                        lightOn(2);
+                    } else {
+                        lightOff(2);
+                    }
                 }
-            }
-        });
+            });
 
-        led3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
+            led3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                                             boolean isChecked) {
 
-                if (isChecked) {
-                    lightOn(3);
-                } else {
-                    lightOff(3);
+                    if (isChecked) {
+                        lightOn(3);
+                    } else {
+                        lightOff(3);
+                    }
                 }
-            }
-        });
+            });
 
-    }//end of oncreate
+        }//end of oncreate
+
+        }
 
     private void closeConnection() {
         try {
@@ -227,19 +241,34 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 socket = new Socket(InetAddress.getByName(ip), port);
+//                Toast.makeText(getApplicationContext(), "socket made", Toast.LENGTH_SHORT).show();
+                System.out.println("Created Socket");
                 out = new ObjectOutputStream(socket.getOutputStream());
+                //System.out.println("Created out");
+//                Toast.makeText(getApplicationContext(), "output stream made", Toast.LENGTH_SHORT).show();
                 out.flush();
-                in = new ObjectInputStream(socket.getInputStream());
-                for (int i = 0; i < 1; i++) {
-                    System.out.println((String) in.readObject() + "\n");
-                }
-                checkSwitchStatus();
-                handler.post(new Runnable() {
-                    public void run() {
-                        connect.setText("Close");
-                        changeSwitchesSatte(true);
-                    }
-                });
+//                System.out.println("flush");
+
+//                Toast.makeText(getApplicationContext(), "output stream flushed", Toast.LENGTH_SHORT).show();
+                //try {
+                    //InputStream testIn = socket.getInputStream();
+                    //ObjectInputStream in = new ObjectInputStream(testIn);
+//                    System.out.println("Created in");
+                    //                Toast.makeText(getApplicationContext(), "input stream made", Toast.LENGTH_SHORT).show();
+//                    System.out.println((String) in.readObject() + "\n");
+                    checkSwitchStatus();
+                    handler.post(new Runnable() {
+                        public void run() {
+                            connect.setText("Close");
+                            changeSwitchesSatte(true);
+                        }
+                    });
+                //}catch() {}
+//                catch(SocketTimeoutException e){
+//                    e.printStackTrace();
+//                } catch(StreamCorruptedException e) {
+//                    e.printStackTrace();
+//                }
             } catch (IOException e) {
                 e.printStackTrace();
                 handler.post(new Runnable() {
@@ -247,9 +276,10 @@ public class MainActivity extends AppCompatActivity {
                         showErrorsMessages("Unknown host!!");
                     }
                 });
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
             }
+//            catch (ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
 
         }
 
