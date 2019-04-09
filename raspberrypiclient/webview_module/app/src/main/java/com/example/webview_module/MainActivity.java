5a648cd0d25d4b74908c413955740514f2a6268b
sancher6 <sancher6@tcnj.edu>
2019-04-09 13:45:35 -0400

    Added Webview Module for Camera Live Stream

---
+++ raspberrypiclient/webview_module/app/src/main/java/com/example/webview_module/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.webview_module;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://192.168.4.1:8000/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
